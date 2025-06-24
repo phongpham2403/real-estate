6feece8049bdacd78b077e3b08adeb43a887fdfb
phongpham240300 <phongpham240300@gmail.com>
2025-06-24 20:02:37 +0700

    create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
 
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
 function App() {
 
   return (
-    <>
-      <h1 className="text-red-500">Hello</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+    return (
+        <div className="text-red-100">Home</div>
+    )
+}
+
+export default Home
